3126053865610e2cc4ac294820931a4f285e5308
Wendy Odia <wodia@SamaDc.corp>
2017-10-28 23:30:01 +0300

    Add binary search class

---
+++ binarySearch.py
@@ -0,0 +1,71 @@
+class binarySearch(list):
+
+  """Boiler plate for binary search class"""
+  def __init__(self,size,step):
+       self.size = size
+       self.step = step
+
+       #Generate list based on size supplied
+       if self.size in self.toTwenty():
+           self.number_list = self.toTwenty()
+
+       elif self.size in self.toForty():
+           self.number_list = self.toForty()
+
+       elif self.size in self.toOneThousand():
+           self.number_list = self.toOneThousand()
+
+       self.length = len(self.number_list)
+
+   #This method will enable us return a list from the class instantiation.
+   #Helped me make sense of these two lines of code:
+   #self.one_to_twenty = binarySearch(20, 1)
+   #self.one_to_twenty[0]
+  def __getitem__(self,index):
+       return self.number_list[index]
+
+
+   #Methods to return lists using list comprehension
+  def toTwenty(self):
+       return [x for x in range(1,21)]
+  def toForty(self):
+       return [x for x in range(2,41,2)]
+  def toOneThousand(self):
+       return [x for x in range(10,1001,10)]
+
+   #Binary search function
+  def search(self,number):
+       firstIndex = 0
+       lastIndex = len(self.number_list) - 1 # 19
+       count = 0
+       index = -1
+       found = False
+
+       #First test if number we are looking for is on first index
+       #Test if it's on the last index
+       #If found, no need to loop
+       if number == self.number_list[firstIndex]:
+           index = firstIndex
+           found = True
+       elif number == self.number_list[lastIndex]:
+           found = True
+           index = lastIndex
+
+       #Here the number we are looking for is not on the first or last index
+       #Halve our list and check if the number we are looking for is at the mid-point
+       #Keep halving and check if number is equal to midpoint
+       while found == False and firstIndex <= lastIndex:
+           count += 1
+           midpoint = (firstIndex + lastIndex) // 2
+
+           if number == self.number_list[midpoint]:
+               found = True
+               return {'count': count, 'index': midpoint}
+           elif self.number_list[midpoint] < number:
+               firstIndex = midpoint + 1
+           else:
+               lastIndex = midpoint - 1
+       #Return -1 as index by default
+       return {'count': 0, 'index': index}
+a = binarySearch(20,2)
+print (a.toForty())
